e8ac8174c706a6338b4245e3951d901d8e2fd738
Talha Yılmaz <talhayilmazc@gmail.com>
2026-05-04 22:50:13 +0300

    chore: initial project setup with Spring Boot 3, Java 17

---
+++ src/main/java/com/secureauth/secureauth/SecureauthApplication.java
@@ -0,0 +1,13 @@
+package com.secureauth.secureauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secureauth/secureauth/SecureauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secureauth.secureauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
