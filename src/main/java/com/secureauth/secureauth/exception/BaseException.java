09e815f9c86f54f8eaadc6e09c826334933086be
Talha Yılmaz <talhayilmazc@gmail.com>
2026-05-08 09:22:37 +0300

    feat: add BaseException

---
+++ src/main/java/com/secureauth/secureauth/exception/BaseException.java
@@ -0,0 +1,17 @@
+package com.secureauth.secureauth.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class BaseException extends RuntimeException {
+
+    private final HttpStatus status;
+    private final String errorCode;
+
+    public BaseException(String message, HttpStatus status, String errorCode) {
+        super(message);
+        this.status = status;
+        this.errorCode = errorCode;
+    }
+}
