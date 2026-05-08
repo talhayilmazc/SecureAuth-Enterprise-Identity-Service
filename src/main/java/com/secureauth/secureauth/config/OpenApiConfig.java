3effb9a60718c6f1b07c29d4d06e9018a6cc6c56
Talha Yılmaz <talhayilmazc@gmail.com>
2026-05-08 14:23:18 +0300

    feat: add OpenApiConfig with JWT security scheme

---
+++ src/main/java/com/secureauth/secureauth/config/OpenApiConfig.java
@@ -0,0 +1,34 @@
+package com.secureauth.secureauth.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SecureAuth API")
+                        .description("Enterprise Identity & Authorization Service")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Talha Yılmaz")
+                                .email("talhayilmazc@gmail.com")))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
