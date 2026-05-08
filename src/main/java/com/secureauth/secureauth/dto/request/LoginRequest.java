a6d5171327d87a119c0fa4134242f55b78eff29b
Talha Yılmaz <talhayilmazc@gmail.com>
2026-05-08 15:39:21 +0300

    feat: add LoginRequest DTO

---
+++ src/main/java/com/secureauth/secureauth/dto/request/LoginRequest.java
@@ -0,0 +1,18 @@
+package com.secureauth.secureauth.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginRequest {
+
+    @NotBlank(message = "Kullanıcı adı boş olamaz")
+    private String username;
+
+    @NotBlank(message = "Şifre boş olamaz")
+    private String password;
+}
