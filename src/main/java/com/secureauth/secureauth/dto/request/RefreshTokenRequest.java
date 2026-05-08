6959407c421a73e467a75f12d5975434689a4396
Talha Yılmaz <talhayilmazc@gmail.com>
2026-05-08 15:49:49 +0300

    feat: add RefreshTokenRequest DTO

---
+++ src/main/java/com/secureauth/secureauth/dto/request/RefreshTokenRequest.java
@@ -0,0 +1,15 @@
+package com.secureauth.secureauth.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RefreshTokenRequest {
+
+    @NotBlank(message = "Refresh token boş olamaz")
+    private String refreshToken;
+}
